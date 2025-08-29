34733a0721fc9a58526922ef3f800810486bfb0f
Mirjahon <mirjahonqochqorov@gmail.com>
2025-08-29 10:12:03 +0500

    salom

---
+++ main.py
@@ -0,0 +1,14 @@
+n = input('Ser?')
+
+
+yangililar = ['Ukraina Rossiyaning harbiy zavodlarini vayron qilishi mumkin', 'Bojni iste’molchi to‘lamoqda. Noqonuniy importga zarba iPhone narxlarini oshirdi']
+
+s = str(input('uni oshirmoqchimisiz? '))
+if s == 'ha':
+    print('bugungi vazifalar')
+else:
+    inp = input('boshqa biror nima qilishni hohlaysizmi? ')
+    if inp == 'ha':
+        print('njiwnjxkslnjxkdw')
+    else:
+        inp = input('yangiliklarni aytib beraymi ser? ')
